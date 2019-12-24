e51f422e56d88ba53cf8495bfbe74ad80a585ac4
Shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-12-25 01:29:27 +0600

    :ok_hand: Online declaration
    Make Display arrow function as oneline declaration

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Display = ({ result }) => (
-  <div>{ result }</div>
-);
+const Display = ({ result }) => <div>{ result }</div>;
+
 
 Display.defaultProps = {
   result: '0',
