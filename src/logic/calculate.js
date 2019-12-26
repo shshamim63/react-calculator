6eafcf30e05bfb8fa1350b983adeecda9cacfc91
Shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-12-26 22:27:53 +0600

    :art: Adds logic to component
    Adds calculator module to control the operation

---
+++ src/logic/calculate.js
@@ -0,0 +1,19 @@
+const calculate = ({ total, next, operation }, btnName) => {
+  switch (btnName) {
+    case 'AC':
+      break;
+    case '=':
+      break;
+    case '+/-':
+      break;
+    case '/':
+    case 'x':
+    case '-':
+    case '+':
+    case '%':
+      break;
+    default:
+      break;
+  }
+};
+export default calculate;
